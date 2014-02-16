36f9dd9b89f14f469c0fba2f3fe49dd19f99aa5d
jesuschicano <jesuschicano3988@gmail.com>
2014-02-16 11:39:39 +0100

    subo todo otra vez

--- conexion.php
+++ conexion.php
@@ -3,7 +3,6 @@ function conectaDb()
 {
 	try{
 		$db = new PDO("mysql:host=localhost;dbname=SabioGC", "root", "root");
-		$db->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
 		return($db);
 	}catch(PDOException $e){
 		echo "No se ha podido conectar a la BD";
