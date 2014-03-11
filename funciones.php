100a55e3587c22e71a99c420a8cd0cee13e9edba
David Moreno <davidml91dml@gmail.com>
2014-03-11 18:04:01 +0100

    Cambios css botones y registro, categorias del experto y cambios anteriores

--- funciones.php
+++ funciones.php
@@ -5,6 +5,7 @@
 	 
 	function conectaDb()
 	{
+		$params = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
 		try{
 			$db = new PDO("mysql:host=localhost;dbname=SabioGC", "usuarios", "us_161213");
 			return($db);
